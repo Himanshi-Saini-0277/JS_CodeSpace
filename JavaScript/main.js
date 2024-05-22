57613592c60c1d9f9615c118bf2e632cabeaa715
Himanshi-Saini-0277 <128921717+Himanshi-Saini-0277@users.noreply.github.com>
2024-05-22 18:04:00 +0000

    conversions

---
+++ JavaScript/main.js
@@ -0,0 +1,25 @@
+console.log("Himanshi")
+
+const b = 3
+const c = String (b)
+
+let x = 1
+let y = Boolean(x)
+let z = Number(x)
+let a = String(x)
+let g = BigInt(x)
+let h;
+let i = String(h)
+let d = false
+let e = Number (d)
+
+x = 6
+
+m = -x
+let n = (3+2)*2+12
+
+k = typeof (x)
+l= typeof(g)
+console.table ([b,c,y,z,a,x,e,g,h,i,k,l,m,n])
+
+document.write(x)
