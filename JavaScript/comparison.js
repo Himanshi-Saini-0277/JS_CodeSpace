c526e7a176384ea9fa77e3c5eaa7945909c7ce96
Himanshi-Saini-0277 <128921717+Himanshi-Saini-0277@users.noreply.github.com>
2024-05-23 09:10:29 +0000

    comp

---
+++ JavaScript/comparison.js
@@ -0,0 +1,18 @@
+console.log ("2">1)
+console.log ("1">2)
+
+console.log("H">5)
+console.log(5>"H")
+console.log(5 == "H")
+
+console.log ("a" > "c")
+
+console.log(null>-1)
+console.log(null>0)
+console.log(null == 0)
+console.log(null>= 0)
+
+console.log("">1)
+console.log(1>"")
+
+console.log(undefined == "")
---
+++ JavaScript/var&dt.js
@@ -0,0 +1,40 @@
+console.log("Himanshi");
+
+const func = function(){
+    console.log ("Hi, I am HImanshi");
+}
+
+const v = Symbol('123')
+const w = Symbol('Hi')
+
+const heros =["Shaktiman", 'Naagraj', 'Doga']
+
+let obj = {
+    Name: "Himanshi",
+    Age: 19
+}
+
+console.log(typeof func)
+console
+
+const b = 3
+const c = String (b)
+
+let x = 1
+let y = Boolean(x)
+let z = Number(x)
+let a = String(x)
+let g = BigInt(x)
+let h;
+let i = String(h)
+let d = false
+let e = Number (d)
+
+x = 6
+
+m = -x
+let n = (3+2)*2+12
+
+k = typeof (x)
+l= typeof(g)
+console.table ([b,c,y,z,a,x,e,g,h,i,k,l,m,n,v,w,heros,obj,func]);
