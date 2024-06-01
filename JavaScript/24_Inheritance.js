33a14e13a3dc70ad973cbdfd83b610393aa7a06f
Himanshi-Saini-0277 <128921717+Himanshi-Saini-0277@users.noreply.github.com>
2024-06-01 12:24:33 +0000

    bind

--- JavaScript/24_Inheritance.js
+++ JavaScript/24_Inheritance.js
@@ -31,3 +31,44 @@ console.log(c.__proto__)
 // Modern Syntax
 let x = Object.setPrototypeOf(b,a)
 console.log(x)
+
+
+// Main Inheritance
+
+class p{
+    constructor (username){
+        this.username = username
+    }
+
+    fromp(){
+        return (`from p ${this.username}`)
+    }
+
+    static something(){ // This will neither work for parent class nor for inherited class because of static
+        return 0
+    }
+}
+
+
+class q extends p {
+    constructor (username, age, course){
+        super(username)
+        this.age = age
+        this.course = course
+    }
+
+    fromq(){
+        return (`from q ${this.username}`)
+    }
+}
+
+let r = new q ("Himanshi", 19 , "B.tech")
+let s = new p ("Parshav")
+
+console.log(r)
+console.log(r.fromp())
+console.log(r.fromq())
+
+console.log(s)
+console.log(s.fromp())
+// console.log(s.fromq()) // do not have access 
